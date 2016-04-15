cb95f8e7ea19aede237907a584378453cf54f888
jim Andresen <jim@wilshirelawfirm.com>
2016-04-15 10:22:53 -0700

    csrf token

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -64,6 +64,7 @@
 		        </div>
 
 				<div class="pure-control-group">
+					<input type="hidden" name="_token" value="{{ csrf_token() }}">
 					{{ csrf_field() }}
 		            <button type="submit" class="pure-button pure-button-primary pure-input-1">Submit</button>
 				</div>
